4e9856813ac318719c5fa802f081f6793e25a07e
Matt Cholick <cholick@gmail.com>
2017-03-01 14:45:40 -0800

    Iterating on build script

--- ci/tasks/configure-ert.sh
+++ ci/tasks/configure-ert.sh
@@ -3,6 +3,38 @@ set -e
 
 json_file="json_file/ert.json"
 
+function fn_om_linux_curl {
+
+    local curl_method=${1}
+    local curl_path=${2}
+    local curl_data=${3}
+
+     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
+            --username \"$pcf_opsman_admin\" \
+            --password \"$pcf_opsman_admin_passwd\"  \
+            curl \
+            --request ${curl_method} \
+            --path ${curl_path}"
+
+    if [[ ! -z ${curl_data} ]]; then
+       curl_cmd="${curl_cmd} \
+            --data '${curl_data}'"
+    fi
+
+    echo ${curl_cmd} > /tmp/rqst_cmd.log
+    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
+
+    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
+      echo "Error Call Failed ...."
+      echo "${curl_method}" "${curl_path}"
+      echo "${curl_data}"
+      echo $(cat /tmp/rqst_stderr.log)
+      exit 1
+    else
+      echo $(cat /tmp/rqst_stdout.log)
+    fi
+}
+
 
 # Setup OM Tool
 sudo cp tool-om/om-linux /usr/local/bin
@@ -50,39 +82,6 @@ if [[ ! -f ${json_file} ]]; then
   echo "Error: cant find file=[${json_file}]"
   exit 1
 fi
-
-function fn_om_linux_curl {
-
-    local curl_method=${1}
-    local curl_path=${2}
-    local curl_data=${3}
-
-     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
-            --username \"$pcf_opsman_admin\" \
-            --password \"$pcf_opsman_admin_passwd\"  \
-            curl \
-            --request ${curl_method} \
-            --path ${curl_path}"
-
-    if [[ ! -z ${curl_data} ]]; then
-       curl_cmd="${curl_cmd} \
-            --data '${curl_data}'"
-    fi
-
-    echo ${curl_cmd} > /tmp/rqst_cmd.log
-    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
-
-    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
-      echo "Error Call Failed ...."
-      echo "${curl_method}" "${curl_path}"
-      echo "${curl_data}"
-      echo $(cat /tmp/rqst_stderr.log)
-      exit 1
-    else
-      echo $(cat /tmp/rqst_stdout.log)
-    fi
-}
-
 
 
 echo "=============================================================================================="

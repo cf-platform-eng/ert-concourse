7c0b27ddd7ae19c7cecd484a002835c1c5168fcd
Todd Ritchie <tritchie+github@pivotal.io>
2018-02-14 13:01:11 -0500

    On gcp: Set credhub instance count to 1, add sql instance CA to template

--- scripts/iaas-specific-config/gcp/run.sh
+++ scripts/iaas-specific-config/gcp/run.sh
@@ -20,11 +20,19 @@ gcloud config set compute/region $gcp_region
 gcloud_sql_instance_cmd="gcloud sql instances list --format json | jq '.[] | select(.instance | startswith(\"${terraform_prefix}\")) | .instance' | tr -d '\"'"
 gcloud_sql_instance=$(eval ${gcloud_sql_instance_cmd})
 gcloud_sql_instance_ip=$(gcloud sql instances list | grep ${gcloud_sql_instance} | awk '{print$4}')
+gcloud_sql_instance_ca=$(gcloud sql instances describe ${gcloud_sql_instance} --format=json | jq .serverCaCert.cert)
+# Strip leading and trailing quotation marks from jq output, leaving in newline escapes
+gcloud_sql_instance_ca="${gcloud_sql_instance_ca%\"}"
+gcloud_sql_instance_ca="${gcloud_sql_instance_ca#\"}"
+
 perl -pi -e "s/{{gcloud_sql_instance_ip}}/${gcloud_sql_instance_ip}/g" ${json_file}
 perl_cmd="perl -pi -e \"s/{{gcloud_sql_instance_username}}/${pcf_opsman_admin}/g\" ${json_file}"
   perl_cmd=$(echo $perl_cmd | sed 's/\@/\\@/g')
   eval $perl_cmd
 perl_cmd="perl -pi -e \"s/{{gcloud_sql_instance_password}}/${pcf_opsman_admin_passwd}/g\" ${json_file}"
+  perl_cmd=$(echo $perl_cmd | sed 's/\@/\\@/g')
+  eval $perl_cmd
+perl_cmd="perl -pi -e \"s/{{gcloud_sql_instance_ca}}/${gcloud_sql_instance_ca}/g\" ${json_file}"
   perl_cmd=$(echo $perl_cmd | sed 's/\@/\\@/g')
   eval $perl_cmd
 

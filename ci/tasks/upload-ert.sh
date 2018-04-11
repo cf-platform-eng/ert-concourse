855ed56337d9de8ef31f7f8cedaac95325628a82
guidowb <guidowb@gmail.com>
2018-04-10 20:54:28 -0500

    Allow Small Runtime

--- ci/tasks/upload-ert.sh
+++ ci/tasks/upload-ert.sh
@@ -14,7 +14,7 @@ om-linux --target https://opsman.$pcf_ert_domain -k \
        --username "$pcf_opsman_admin" \
        --password "$pcf_opsman_admin_passwd" \
       upload-product \
-      --product pivnet-elastic-runtime/cf*.pivotal
+      --product pivnet-elastic-runtime/*.pivotal
 
 ##Get Uploaded Tile --product-version
 

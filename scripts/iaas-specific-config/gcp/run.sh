7acc7daf15a4366cd56da5b32c6d3ba56aa59c31
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2018-06-21 13:22:05 -0400

    Fix parsing of new sql api data
    Signed-off-by: Todd Ritchie <tritchie@pivotal.io>

--- scripts/iaas-specific-config/gcp/run.sh
+++ scripts/iaas-specific-config/gcp/run.sh
@@ -17,9 +17,9 @@ gcloud config set compute/region $gcp_region
 #############################################################
 # get GCP unique SQL instance ID & set params in JSON       #
 #############################################################
-gcloud_sql_instance_cmd="gcloud sql instances list --format json | jq '.[] | select(.instance | startswith(\"${terraform_prefix}\")) | .instance' | tr -d '\"'"
+gcloud_sql_instance_cmd="gcloud sql instances list --format json | jq '.[] | select(.name | startswith(\"${terraform_prefix}\")) | .name' | tr -d '\"'"
 gcloud_sql_instance=$(eval ${gcloud_sql_instance_cmd})
-gcloud_sql_instance_ip=$(gcloud sql instances list | grep ${gcloud_sql_instance} | awk '{print$4}')
+gcloud_sql_instance_ip=$(gcloud sql instances list | grep ${gcloud_sql_instance} | awk '{print$5}')
 gcloud_sql_instance_ca=$(gcloud sql instances describe ${gcloud_sql_instance} --format=json | jq .serverCaCert.cert)
 # Strip leading and trailing quotation marks from jq output, leaving in newline escapes
 gcloud_sql_instance_ca="${gcloud_sql_instance_ca%\"}"
